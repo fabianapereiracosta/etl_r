9b24df0ed7be595e01f06ecf77ddcb1e067f30ba
fabianapereiracosta <fabiana.pereiracosta@ufpe.br>
2021-04-11 07:53:50 -0300

    obj_func

---
+++ scripts/obj_e_func.R
@@ -0,0 +1,19 @@
+#Kaplan-Meier da evasão entre os ingressantes 2011 nas Universidades Federais
+
+require(survival)
+
+
+#Base de dados composta apenas pelos estudantes que declararam cor/raça
+
+KM1<-read.csv(file="cor_raca.csv", header=TRUE, sep=";")
+attach(KM1)
+
+#A função Surv() define o objeto do tipo tempo de sobrevivência
+Surv(TEMPO,EVASAO)
+plot(Surv)
+str(Surv)
+
+#A função survfit() estima a curva de sobrevivência
+ekm<-survfit(Surv(KM$TEMPO, KM$EVASAO)~1)
+plot(ekm)
+str(ekm)
